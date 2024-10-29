af206588dd4b3908d39fc81e0d5779316ec5d9c2
Nadim201 <nadimmahmud203201@gmail.com>
2024-10-29 22:22:38 +0600

    update

--- lib/data/common/utils.dart
+++ lib/data/common/utils.dart
@@ -1,5 +1,5 @@
 class Utils {
-  static const String baseUrl = 'http://152.42.163.176:2006/api/v1';
+  static const String baseUrl = 'http://35.73.30.144:2005/api/v1';
   static const String registration = '$baseUrl/Registration';
   static const String login = '$baseUrl/Login';
   static const String forgot = '$baseUrl/RecoverVerifyEmail/';
